a54f27ec3b96767762ca682ac89bc5d3cd506088
niculas1989 <nicolas.maranzano@libero.it>
2022-03-17 15:15:54 +0100

    added more changes

--- resources/views/Movies/index.blade.php
+++ resources/views/Movies/index.blade.php
@@ -4,12 +4,12 @@
 
 @section('content')
 @forelse($movies as $m)
-<div class="card d-flex flex-wrap">
-    <div class="card-body">
+<div class="card">
+    <div class="card-body w-25">
         <h5 class="card-title">Titolo: {{$m->title}}</h5>
         <h6 class="card-subtitle mb-2 text-muted">Titolo originale: {{$m->original_title}}</h6>
         <p class="card-text">Nationality: {{$m->nationality}}</p>
-        <p><i>Data d'uscita: {{$m->date}}</i>></p>
+        <p><i>Data d'uscita: {{$m->date}}</i></p>
         <a href="#" class="card-link">Card link</a>
     </div>
 </div>

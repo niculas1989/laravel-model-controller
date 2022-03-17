5156cf9fa6bfe3fd553aaf8a1c8d664bc973fad6
niculas1989 <nicolas.maranzano@libero.it>
2022-03-17 14:14:59 +0100

    added changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,16 +12,10 @@
 <body>
     <ul class="nav justify-content-center">
         <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Active</a>
+            <a class="nav-link" href="#">HOME</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="#">ALL FILMS</a>
         </li>
     </ul>
 </body>
